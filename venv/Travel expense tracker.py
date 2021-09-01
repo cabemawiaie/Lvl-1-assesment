5b5411a55c80659ff96a83c9eb05134c8065916a
cabemawiaie <72894733+cabemawiaie@users.noreply.github.com>
2021-09-01 18:45:44 +1200

    Update Travel expense tracker.py
    Attempted to fix negative number error by adding if statement

--- venv/Travel expense tracker.py
+++ venv/Travel expense tracker.py
@@ -1,12 +1,19 @@
 #Year 11 assesment task
 def force_number(message):
-	while True:
-		try:
-			number = float(input(message))
-			break
-		except ValueError:
-			print("Please enter a valid number e.g. 200") 
-	return ("{:,.2f}".format(number))
-
+    while True:
+        try:
+            number = float(input(message))
+            break
+        except ValueError:
+            print("Please enter a number")
+            continue
+    
+        if number < 0:
+            print("Please enter an amount greater than 0")
+            continue
+        else:
+             break
+    return ("${:,.2f}".format(number))
+    
 daily_budget = force_number("What is your daily budget?")
 print(daily_budget) 

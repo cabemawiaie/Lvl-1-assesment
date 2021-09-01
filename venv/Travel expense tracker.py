be793e05343c08e12824162a053237c683917b19
cabemawiaie <72894733+cabemawiaie@users.noreply.github.com>
2021-09-01 17:25:59 +1200

    Update Travel expense tracker.py
    Code does not  do everything that is expected from it.  It accepts negative numbers

--- venv/Travel expense tracker.py
+++ venv/Travel expense tracker.py
@@ -1,1 +1,12 @@
 #Year 11 assesment task
+def force_number(message):
+	while True:
+		try:
+			number = float(input(message))
+			break
+		except ValueError:
+			print("Please enter a valid number e.g. 200") 
+	return ("{:,.2f}".format(number))
+
+daily_budget = force_number("What is your daily budget?")
+print(daily_budget) 

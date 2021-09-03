a841d44e5981402a75dbb55835143d8ad86cdc1b
cabemawiaie <72894733+cabemawiaie@users.noreply.github.com>
2021-09-03 14:14:26 +1200

    Update Travel expense tracker.py
    Fixed acceptance of negative value

--- venv/Travel expense tracker.py
+++ venv/Travel expense tracker.py
@@ -3,17 +3,17 @@ def force_number(message):
     while True:
         try:
             number = float(input(message))
-            break
+            if(number <= 0):
+               print("Please enter an amount greater than $0")
+               continue
         except ValueError:
-            print("Please enter a number")
-            continue
-    
-        if number < 0:
-            print("Please enter an amount greater than 0")
+            print("Please enter an amount")
             continue
         else:
-             break
+            print("Thank you")
+        break
     return ("${:,.2f}".format(number))
+
     
 daily_budget = force_number("What is your daily budget?")
-print(daily_budget) 
+print("You have set your daily budget to {}".format(daily_budget))

9d76e47a84549db7d75218b298d961d5e1db9852
cabemawiaie <72894733+cabemawiaie@users.noreply.github.com>
2021-09-03 16:19:38 +1200

    Added confirm answer function

--- venv/Travel expense tracker.py
+++ venv/Travel expense tracker.py
@@ -14,6 +14,23 @@ def force_number(message):
         break
     return ("${:,.2f}".format(number))
 
-    
+def confirm_answer(question):
+    while True:
+        reply = input(question).lower().strip()
+        if reply == "yes":
+            return True
+            break
+        elif reply == "no":
+            return False
+            break
+        else:
+            print("Please enter yes or no")
+            continue
+   
 daily_budget = force_number("What is your daily budget?")
 print("You have set your daily budget to {}".format(daily_budget))
+budget_confirmation = confirm_answer("Are you sure this is your budget? [yes or no]")
+if budget_confirmation == False:
+    daily_budget = force_number("What is your daily budget then?")
+elif budget_confirmation == True:
+    print ("Thank you, you may proceed") 

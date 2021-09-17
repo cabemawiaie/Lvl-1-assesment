2b3c4c23bc3c0736ab6fc502fcbb1786b4435eda
cabemawiaie <72894733+cabemawiaie@users.noreply.github.com>
2021-09-17 13:28:23 +1200

    History function added
    Category question repeats unnecessarily and all expenses are entered into the food dictionary

--- venv/Travel expense tracker.py
+++ venv/Travel expense tracker.py
@@ -56,22 +56,61 @@ def category():
 category()
 
          
+
 def continue_spending():
     tracking = True
     expenses = {}
     while tracking:
         #Prompt for user's expense and expense type
+        category(category_list)
         name = input("What is your expense for? e.g breakfast, movies, taxi  ").strip().title()
-        expense = float(input("Enter your expense:$"))
+        expense = force_number("Enter your expense:$")
         #Stores expense in the dictionary
         expenses[name] = expense
         #Finding out if user wants to enter another expense
         repeat = confirm_answer("Would you like to enter another expense?")
-        if repeat == "no":
+        if repeat == False:
             tracking = False
-            history = confirm_answer("Would you like to see your expense history?")
-            if tracking == False and history == True:
-               print("~ Payment history ~")
-               for name, expense in expenses.items():
-                  print(f"{name}: ${expense}")
+            return expenses
 
+def history(Chosen_Category, User_Expenses):
+    history = confirm_answer("Would you like to see your expense history?")
+    while history == True:
+        if Chosen_Category == "food":
+            print("~ Food expenditure ~")
+            food = User_Expenses
+            print(food)
+        elif Chosen_Category == "travel":
+            print("~ Travel expenditure ~")
+            travel = User_Expenses
+            print(travel)
+        elif Chosen_Category == "accommodation":
+            print("~ Accommodation expenditure ~")
+            accommodation = User_Expenses
+            print(accommodation)
+        elif Chosen_Category == "activity":
+            print("~ Activity expenditure ~")
+            activity = User_Expenses
+            print(activity)
+        elif Chosen_Category == "miscellaneous":
+            print("~ Miscellaneous expenditure ~")
+            miscellaneous = User_Expenses
+            print(miscellaneous)
+        else:
+            print("Thank you, for using this program")
+
+
+#MAIN
+
+daily_budget = force_number("What is your daily budget: $")
+print("You have set your daily budget to {}".format(daily_budget))
+budget_confirmation = confirm_answer("Are you sure this is your budget? [yes or no]")
+while budget_confirmation == False:
+    daily_budget = force_number("What is your daily budget?")
+    budget_confirmation = confirm_answer("Are you sure this is your budget? [yes or no]")
+else:
+    print("Thank you, you may proceed")
+
+Chosen_Category = category(category_list)
+User_Expenses = continue_spending()
+history(Chosen_Category, User_Expenses)
